ac8cdd418fe27a7f35b4508ae698961d929c4e8f
Nigel Sheridan-Smith <nigel@greenshoresdigital.com>
2020-06-07 09:43:07 +1000

    Look at watchlist

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,17 +1,50 @@
 class MarketsController < ApplicationController
+  def index
+    @watchlist = watchlist
+    date = DateTime.new(2020,01,31)
+    @dailies = DailySummary.includes(:company).where(symbol: @watchlist, date: date).order(change_pct: :desc)
+  end
+
   def show
-    start_at = 90.days.ago
-    @company = Company.includes(:dailies, :daily_summaries, :chains).find_by(symbol: params[:id])
-    @dailies = @company.dailies.where('date >= ?', start_at)
-    @daily_summaries = @company.daily_summaries.where('date >= ?', start_at)
+    start_at = 6.months.ago
+    @company = Company.find_by(symbol: params[:id]) # .includes(:dailies, :daily_summaries, :chains)
+    @dailies = @company.dailies.where('date >= ?', start_at).order(date: :desc)
+    @daily_summaries = @company.daily_summaries.where('date >= ?', start_at).order(date: :desc)
     @chains = @company.chains.where('start_at >= ? or finish_at >= ?', start_at, start_at)
   end
 
   def crypto_market_cap
-    start_at = 90.days.ago
+    start_at = 6.months.ago
     @cryptos = CryptoCurrency.includes(:cryptos).joins(:cryptos)
                              .where('cryptos.date >= ?', start_at)
                              .where('cryptos.rank <= ?', 150)
-                             .order('cryptos.rank ASC, cryptos.date DESC')
+                             .order('cryptos.date DESC, cryptos.rank ASC')
+  end
+
+  def watchlist
+    %w[QAN RRL MTS LVH DTC BRN DSE NEC
+                              WSP CKF Z1P TYR
+                              ADH PCK
+                              KGN AQG NXT REA PPH EVN AKP OGC CDD MML PRX EQT PRU
+                              SZL SWF MA1 OPY IXC APT JIN JBH JHG RZI
+                              DXB HVM XTE IFL VRX G1A
+
+                              MEM FXL PPE CXO PPK PDN PDL SUL AQZ SLK HAW RHT PNI
+                              HM1 PMV QVE CAN HMC VMT EZL PIC MOT MDI RKN EGG INA
+                              ALL CMP WTC CTD CDV AGH ATL MOE FHS MMJ AZY EOF 14D
+                              AVN TAH BGA CQE SCG LIC MVP WES BTI ORG GNE LSF PPT
+                              HLO TCL WPL MFF SEK MME TLX STO HPI PBH SKC BRV AMX RMC
+
+                              LON PFP CGB PAR AVL PWG BUB HIT TIE CBR SND AEF
+                              CGF FLN MFG YAL KWR TLT PSQ APX RBL OBM TNE PL8
+                              BCN BCB CYL OKU AHZ WND CAM ALU CUP NXS MXT IRE
+                              IRI TDI NAN BRG HTA HRL MDR EVO S2R CZR ENR HGO
+                              UBN POS WQG WEC NAG PAB TAM PAA PG1 EBO STG ELO
+                              ELS M7T TGG MAU FML BRB PWH SPL TD1 CUV IDT TPW
+                              TGA ANP TTB NVX DMP AUI UWL SMR GOZ WHC WMI CDM
+                              HAV WBT TNY ALG THC GCI AMP SIQ LCL SOR AEG SPT
+                              UBI AHF NOV GMD CCV SPX ARV BSX AGG ZEN PNR TBR
+                              MSB IMC CLQ CT1 ALQ SGF BIN FAR MCA RAP ICE IMU
+                            ]
   end
 end
